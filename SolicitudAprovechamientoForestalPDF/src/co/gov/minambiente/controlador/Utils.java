0bec33dda43eee81e54819a719272d044e41ef0c
AndresG <Eljac@Andres>
2022-08-14 15:39:41 -0500

    Crecion de Utils

--- SolicitudAprovechamientoForestalPDF/src/co/gov/minambiente/controlador/Utils.java
+++ SolicitudAprovechamientoForestalPDF/src/co/gov/minambiente/controlador/Utils.java
@@ -13,11 +13,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,7 +23,7 @@ import java.util.logging.Logger;
  */
 public class Utils {
 
-    public static String[] loadFile(File directory) throws IOException, FileNotFoundException{
+    public static String[] loadFile(File directory) throws IOException, FileNotFoundException {
         int contador = 0;
         String temporal = "";
         String[] cutter;
@@ -62,29 +57,36 @@ public class Utils {
         return cutter;
     }
 
-    
     /**
-     * 
+     *
      * @param tabla Archivo File con la dirección de la tabla a cargar
-     * @return  Retorna una lista de departamentos que tiene a su vez una lista de municipalities
-     * @throws IOException  Excepcion de entrada y salida
+     * @return Retorna una lista de departamentos que tiene a su vez una lista
+     * de municipalities
+     * @throws IOException Excepcion de entrada y salida
      * @throws FileNotFoundException Excepción de archivo no encontrado
      */
-    public static LinkedList loadMunicipalities(File tabla) throws IOException, FileNotFoundException{
+    public static LinkedList<DepartmentModel> loadMunicipalities(File tabla) throws IOException, FileNotFoundException {
 
         LinkedList<String> municipalities = new LinkedList<>();
         LinkedList<DepartmentModel> departments = new LinkedList<>();
         boolean esPrimero = true;
         int contador = 0;
-     
-        BufferedReader br = new BufferedReader(new InputStreamReader(
-                new FileInputStream(tabla), "ISO-8859-1"));
-
+        BufferedReader br = null;
+        
+        try {
+            br = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(tabla), "ISO-8859-1"));
+        }
+        catch(FileNotFoundException e){
+            System.out.println("No se encontró el directorio");
+        }
+  
+        
         String temporal = "";
         String[] cutter;
         String entrada;
         String temporalDepartment = "";
-        
+
         try {
 
             while (((entrada = br.readLine()) != null)) {
@@ -92,11 +94,10 @@ public class Utils {
                 temporal = entrada;
                 cutter = temporal.split("-");
 
-                if(esPrimero){
-                    esPrimero= false;
+                if (esPrimero) {
+                    esPrimero = false;
                     municipalities.add(cutter[1]);
-                }
-                else if (temporalDepartment.equals(cutter[0])) {
+                } else if (temporalDepartment.equals(cutter[0])) {
                     municipalities.add(cutter[1]);
                 } else {
                     departments.add(new DepartmentModel(temporalDepartment, municipalities));
@@ -104,21 +105,24 @@ public class Utils {
                     municipalities.add(cutter[1]);
                 }
                 temporalDepartment = cutter[0];
-                
+
             }
-            
-            departments.add(new DepartmentModel(temporalDepartment, municipalities)); 
+
+            departments.add(new DepartmentModel(temporalDepartment, municipalities));
 
         } catch (IOException ex) {
             Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
         }
-       
-       System.out.println("Cargados los siguientes departamentos:");
-       departments.forEach((department) -> {
-                System.out.println(department.toString());
-            });
-       
-       return departments;
+
+        
+        
+        
+        System.out.println("Cargados los siguientes departamentos:");
+        departments.forEach((department) -> {
+            System.out.println(department.toString());
+        });
+
+        return departments;
     }
 
 }
