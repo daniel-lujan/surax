09d345b6efe2e6f45ce0147563e49d81b99aa326
daniel <daniel@DESKTOP-CEK0K51>
2022-08-17 18:30:03 -0500

    + Modelo para fechas

---
+++ SolicitudAprovechamientoForestalPDF/src/co/gov/minambiente/modelo/DateModel.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.gov.minambiente.modelo;
+
+/**
+ *
+ * @author daniel
+ */
+public class DateModel {
+    private int year, month, day;
+
+    public DateModel() {
+        this.year = 0;
+        this.month = 0;
+        this.day = 0;
+    }
+
+    
+    
+    public DateModel(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+    
+}
